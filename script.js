9b485682b91a26ef656067231d5797782172c003
vannndar <thariq.ivan@gmail.com>
2023-10-16 20:24:34 +0700

    Adding JSON and XML type

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//JSON
 $(document).ready(function() {
 
 
@@ -21,3 +22,33 @@ $(document).ready(function() {
     });
 
 });
+
+// // XML
+// $(document).ready(function() {
+
+
+//     $('form').submit(function() {
+
+//         var link = 'http://api.openweathermap.org/data/2.5/weather?q=' + $('#weather_input').val() + '&mode=xml&appid=2a6969bf1b08c48c4792b6af3e0f1166&lang=id&units=metric';
+
+//         $.get(link, function(weather) {
+//             // const parser = new DOMParser();
+//             // const xmlDoc = parser.parseFromString(weather, "text/xml");
+//             console.log(weather);
+            
+//             $('#lat').text(weather.querySelector("coord").getAttribute("lat"));
+//             $('#long').text(weather.querySelector("coord").getAttribute("lon"));
+//             $('#kota').text(weather.querySelector("city").getAttribute("name")+"-"+weather.querySelector("country").textContent);
+//             $('#min_temp').text(weather.querySelector("temperature").getAttribute("min")+" °C");
+//             $('#max_temp').text(weather.querySelector("temperature").getAttribute("max")+" °C");
+//             $('#temp').text(weather.querySelector("temperature").getAttribute("value")+" °C");
+//             $('#humid').text(weather.querySelector("humidity").getAttribute("value")+"%");
+//             $('#pres').text(weather.querySelector("pressure").getAttribute("value")+" hPa");
+//             $('#cloud').text(weather.querySelector("clouds").getAttribute("name"));
+//             $('#wind').text(weather.querySelector("speed").getAttribute("value")+" m/s");
+            
+//         }, "xml");
+//         return false;
+//     });
+
+// });
